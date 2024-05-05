74de644051fde4bc4e04cbfb100beb3800b814b0
iblea <iblea@iasdf.com>
2024-05-05 23:55:32 +0900

    feat: Add token table

---
+++ sql/table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE token
+(
+    bot_name      VARCHAR(128) NOT NULL,
+    bot_token     VARCHAR(256) NOT NULL,
+    bot_channel   VARCHAR(256) NOT NULL,
+    PRIMARY KEY     (token_name)
+);
