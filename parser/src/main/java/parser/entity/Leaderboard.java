b1b45392a6a4bbe9f0dc384dd985352796df8707
iblea <iblea@iasdf.com>
2024-05-11 23:38:20 +0900

    refactor: Leaderboard 생성자 추가

--- parser/src/main/java/parser/entity/Leaderboard.java
+++ parser/src/main/java/parser/entity/Leaderboard.java
@@ -7,6 +7,22 @@ public class Leaderboard {
     private int score;
     private long parseTime;
 
+    public Leaderboard() {
+        this.rank = -1;
+        this.name = "";
+        this.score = -1;
+        this.parseTime = -1;
+        // this.parseTime = System.currentTimeMillis() / 1000;
+    }
+
+    public Leaderboard(int rank, String name, int score)
+    {
+        this.rank = rank;
+        this.name = name;
+        this.score = score;
+        this.parseTime = -1;
+    }
+
     public int getRank() {
         return this.rank;
     }
