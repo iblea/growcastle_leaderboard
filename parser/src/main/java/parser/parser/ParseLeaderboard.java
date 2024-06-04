31d3663f350e8f0421d3b45fc84b9a84159b1a08
iblea <iblea@iasdf.com>
2024-06-04 19:22:19 +0900

    fix: rank 중복 값에 대한 처리 수정 (rank 직접계산)

--- parser/src/main/java/parser/parser/ParseLeaderboard.java
+++ parser/src/main/java/parser/parser/ParseLeaderboard.java
@@ -12,46 +12,16 @@ import org.json.simple.parser.ParseException;
 
 import parser.entity.Leaderboard;
 
+
+/**
+ * 리더보드 정보를 파싱하는 클래스
+ * player (플레이어), guild (길드), hell (무한웨이브) 리더보드에 대한 응답값을 파싱한다.
+ * 어느 타입이든, 리더보드 타입은 모두 rank, score, name 로 구성되어 있다.
+ */
 public class ParseLeaderboard extends ParseAPI {
 
     public ParseLeaderboard(TelegramBot bot) {
         super(bot);
-    }
-
-    /**
-     * 리턴된 Json을 파싱한다.
-     *
-     * @param leaderboardDataString
-     * @return List<Leaderboard>
-     */
-    public List<Leaderboard> leaderboardJsonParser(String leaderboardDataString)
-        throws ParseException, NullPointerException, WrongJsonType
-    {
-        JSONArray leaderboardlist = getAPIResponseData(leaderboardDataString);
-
-        List<Leaderboard> leaderboards = new ArrayList<Leaderboard>();
-        for (int i = 0; i < leaderboardlist.size(); i++) {
-            JSONObject rankObject = (JSONObject)leaderboardlist.get(i);
-
-            Integer rank = (Integer)rankObject.get("rank");
-            Integer score = (Integer)rankObject.get("score");
-            Leaderboard leaderboard = new Leaderboard(
-                rank.intValue(),
-                (String)rankObject.get("name"),
-                score.intValue()
-            );
-            leaderboards.add(leaderboard);
-        }
-        return leaderboards;
-    }
-
-    /**
-     * 요청할 API URL을 만든다.
-     * @param leaderboardType - 요청할 URL 타입 ( players / guilds )
-     * @return String
-     */
-    private String getLeaderboardURL(String leaderboardType) {
-        return getCurrentKSTURL() + "/" + leaderboardType;
     }
 
     /**
@@ -78,11 +48,60 @@ public class ParseLeaderboard extends ParseAPI {
             leaderboards = leaderboardJsonParser(leaderboardData);
         } catch (ParseException | NullPointerException | WrongJsonType e) {
             e.printStackTrace();
-            sendErrMsg(e.getMessage());
             sendErrMsg("Leaderboard (" + leaderboardType + ") Parse Error : " + e.getMessage());
             return null;
         }
         return leaderboards;
+    }
+
+    /**
+     * 요청할 API URL을 만든다.
+     * @param leaderboardType - 요청할 URL 타입 ( players / guilds )
+     * @return String
+     */
+    private String getLeaderboardURL(String leaderboardType) {
+        return getCurrentURL() + "/" + leaderboardType;
+    }
+
+    /**
+     * 리턴된 Json을 파싱한다.
+     *
+     * @param leaderboardDataString
+     * @return List<Leaderboard>
+     */
+    public List<Leaderboard> leaderboardJsonParser(String leaderboardDataString)
+        throws ParseException, NullPointerException, WrongJsonType
+    {
+        JSONArray leaderboardlist = getAPIResponseData(leaderboardDataString);
+
+        List<Leaderboard> leaderboards = new ArrayList<Leaderboard>();
+        for (int i = 0; i < leaderboardlist.size(); i++) {
+            JSONObject rankObject = (JSONObject)leaderboardlist.get(i);
+            leaderboards.add(getLeaderboardInJson(rankObject, i));
+        }
+        return leaderboards;
+    }
+
+    /**
+     * 리더보드 Json 엔티티 형식을 Leaderboard JDO 로 변환한다.
+     * 랭크는 불가피하게 for문을 순회해가며 순위를 매긴다.
+     * 동일한 웨이브의 경우 이를 동일한 순위로 부여한다.
+     * ex: A유저 100wave, B유저 80wave, C유저 80wave 일 때 B,C 유저의 랭크를 모두 2로 부여한다.
+     * 이 경우 특정 시간에 해당 랭크의 정보가 없을 수 있으므로, 순위 계산에 오류가 생길 수 있다.
+     * 따라서 for문을 순회하며 순위를 매겨 rank를 부여해 동일한 시간에 중복되는 랭크가 없도록 한다.
+     *
+     * @param rankObject - rank json object
+     * @param rank - 리더보드 순위
+     * @return Leaderboard - 리더보드 JDO Entity
+     */
+    Leaderboard getLeaderboardInJson(JSONObject rankObject, int rank) {
+        // Integer rank = (Integer)rankObject.get("rank");
+        Integer score = (Integer)rankObject.get("score");
+        return new Leaderboard(
+            rank + 1,
+            (String)rankObject.get("name"),
+            score.intValue()
+        );
     }
 
 }
