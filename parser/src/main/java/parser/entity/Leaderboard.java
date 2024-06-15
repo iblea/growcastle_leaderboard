0bf545702f0c0eb92b8caea813b4fccf531c2b0d
iblea <iblea@iasdf.com>
2024-06-16 00:31:45 +0900

    feat: Leaderboard 엔티티 DTO화 진행

--- parser/src/main/java/parser/entity/Leaderboard.java
+++ parser/src/main/java/parser/entity/Leaderboard.java
@@ -1,17 +1,42 @@
 package parser.entity;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.SecondaryTable;
+import javax.persistence.SecondaryTables;
+import javax.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Players")
+@SecondaryTables({
+    @SecondaryTable(name = "Guilds"),
+    @SecondaryTable(name = "Hellmode")
+})
 public class Leaderboard {
 
+    @Column(name = "rank")
     private int rank;
+
+    @Id
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "score")
     private int score;
-    private long parseTime;
+
+    @Id
+    @Column(name = "parseTime")
+    private LocalDateTime parseTime;
+    // private long parseTime;
 
     public Leaderboard() {
         this.rank = -1;
         this.name = "";
         this.score = -1;
-        this.parseTime = -1;
         // this.parseTime = System.currentTimeMillis() / 1000;
     }
 
@@ -20,15 +45,13 @@ public class Leaderboard {
         this.rank = rank;
         this.name = name;
         this.score = score;
-        this.parseTime = -1;
     }
 
-    public Leaderboard(int rank, String name, int score, int parseTime)
+    public Leaderboard(int rank, String name, int score, LocalDateTime parseTime)
     {
         this.rank = rank;
         this.name = name;
         this.score = score;
-        this.parseTime = parseTime;
     }
 
     public int getRank() {
@@ -55,11 +78,11 @@ public class Leaderboard {
         this.score = score;
     }
 
-    public long getParseTime() {
+    public LocalDateTime getParseTime() {
         return this.parseTime;
     }
 
-    public void setParseTime(long parseTime) {
+    public void setParseTime(LocalDateTime parseTime) {
         this.parseTime = parseTime;
     }
 }
