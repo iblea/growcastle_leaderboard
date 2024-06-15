67afd8d3594554ff146dafd4ed353e9403e2ac93
iblea <iblea@iasdf.com>
2024-06-16 00:32:39 +0900

    fix: getParseTimeUnix, getParseTime (LocalDateTime) 함수 분리

--- parser/src/main/java/parser/db/Insert.java
+++ parser/src/main/java/parser/db/Insert.java
@@ -5,23 +5,25 @@ import java.time.ZoneId;
 
 public class Insert {
 
-    public Insert() {
 
+    public LocalDateTime getParseTime() {
+        LocalDateTime now = LocalDateTime.now();
+        int minutes = now.getMinute();
+        int roundedMinutes = (minutes / 5) * 5;
+        LocalDateTime roundedTime = now.withMinute(roundedMinutes)
+            .withSecond(0)
+            .withNano(0)
+            .atZone(ZoneId.of("Asia/Seoul"))
+            .toLocalDateTime();
+        return roundedTime;
     }
 
     /**
      * 파싱한 현재 시간을 5분단위로 내려 Unix Time을 리턴한다. (Asia/Seoul - KST 기준)
      * @return long
      */
-    private long getParseTimeUnix() {
-        LocalDateTime now = LocalDateTime.now();
-        int minutes = now.getMinute();
-        int roundedMinutes = (minutes / 5) * 5;
-        LocalDateTime roundedTime = now.withMinute(roundedMinutes).withSecond(0).withNano(0);
-        // UTC
-        // long unixTime = roundedTime.toEpochSecond(java.time.ZoneOffset.UTC);
-        // Asia/Seoul - KST
-        long unixTime = roundedTime.atZone(ZoneId.of("Asia/Seoul")).toEpochSecond();
+    public long getParseTimeUnix() {
+        long unixTime = getParseTime().atZone(ZoneId.of("Asia/Seoul")).toEpochSecond();
         return unixTime;
     }
 }
