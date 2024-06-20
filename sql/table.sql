6210e0d529711a523c717439ddae0dbcfe182bea
iblea <iblea@iasdf.com>
2024-06-20 22:53:25 +0900

    chore: CREATE TABLE SQL 추가

--- sql/table.sql
+++ sql/table.sql
@@ -5,3 +5,33 @@ CREATE TABLE token
     bot_channel   VARCHAR(256) NOT NULL,
     PRIMARY KEY     (token_name)
 );
+
+-- drop table Leaderboard_Player;
+-- show columns from LEADERBOARD_PLAYER;
+
+CREATE TABLE Leaderboard_Player
+(
+    rank        INT NOT NULL,
+    name        VARCHAR(32) NOT NULL,
+    score       INT NOT NULL,
+    parseTime   TIMESTAMP NOT NULL,
+    PRIMARY KEY (name, parseTime)
+);
+
+CREATE TABLE Leaderboard_Guild
+(
+    rank        INT NOT NULL,
+    name        VARCHAR(32) NOT NULL,
+    score       INT NOT NULL,
+    parseTime   TIMESTAMP NOT NULL,
+    PRIMARY KEY (name, parseTime)
+);
+
+CREATE TABLE Leaderboard_Hell
+(
+    rank        INT NOT NULL,
+    name        VARCHAR(32) NOT NULL,
+    score       INT NOT NULL,
+    parseTime   TIMESTAMP NOT NULL,
+    PRIMARY KEY (name, parseTime)
+);
