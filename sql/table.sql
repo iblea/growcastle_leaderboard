b4a96f77dadb9af31b74e5242604aa12e6903171
iblea <iblea@iasdf.com>
2024-08-04 18:41:03 +0900

    chore: table 추가 스크립트

--- sql/table.sql
+++ sql/table.sql
@@ -17,7 +17,6 @@ CREATE TABLE Leaderboard_Player
     parseTime   TIMESTAMP NOT NULL,
     PRIMARY KEY (name, parseTime)
 );
-
 CREATE TABLE Leaderboard_Guild
 (
     rank        INT NOT NULL,
@@ -35,3 +34,101 @@ CREATE TABLE Leaderboard_Hell
     parseTime   TIMESTAMP NOT NULL,
     PRIMARY KEY (name, parseTime)
 );
+
+
+CREATE TABLE `underdog`
+(
+    name        VARCHAR(32) NOT NULL,
+    score       INT NOT NULL,
+    parseTime   TIMESTAMP NOT NULL,
+    PRIMARY KEY (name, parseTime)
+);
+
+CREATE TABLE `underdog`
+(
+    name        VARCHAR(32) NOT NULL,
+    score       INT NOT NULL,
+    parseTime   TIMESTAMP NOT NULL,
+    PRIMARY KEY (name, parseTime)
+);
+
+CREATE TABLE `sayonara`
+(
+    name        VARCHAR(32) NOT NULL,
+    score       INT NOT NULL,
+    parseTime   TIMESTAMP NOT NULL,
+    PRIMARY KEY (name, parseTime)
+);
+
+CREATE TABLE `redbridge`
+(
+    name        VARCHAR(32) NOT NULL,
+    score       INT NOT NULL,
+    parseTime   TIMESTAMP NOT NULL,
+    PRIMARY KEY (name, parseTime)
+);
+
+CREATE TABLE `paragonia`
+(
+    name        VARCHAR(32) NOT NULL,
+    score       INT NOT NULL,
+    parseTime   TIMESTAMP NOT NULL,
+    PRIMARY KEY (name, parseTime)
+);
+
+CREATE TABLE `droplet`
+(
+    name        VARCHAR(32) NOT NULL,
+    score       INT NOT NULL,
+    parseTime   TIMESTAMP NOT NULL,
+    PRIMARY KEY (name, parseTime)
+);
+
+CREATE TABLE `777`
+(
+    name        VARCHAR(32) NOT NULL,
+    score       INT NOT NULL,
+    parseTime   TIMESTAMP NOT NULL,
+    PRIMARY KEY (name, parseTime)
+);
+
+CREATE TABLE `skeleton_skl`
+(
+    name        VARCHAR(32) NOT NULL,
+    score       INT NOT NULL,
+    parseTime   TIMESTAMP NOT NULL,
+    PRIMARY KEY (name, parseTime)
+);
+
+CREATE TABLE `shalom`
+(
+    name        VARCHAR(32) NOT NULL,
+    score       INT NOT NULL,
+    parseTime   TIMESTAMP NOT NULL,
+    PRIMARY KEY (name, parseTime)
+);
+
+-- CREATE TABLE `under dog`
+-- (
+--     name        VARCHAR(32) NOT NULL,
+--     score       INT NOT NULL,
+--     parseTime   TIMESTAMP NOT NULL,
+--     PRIMARY KEY (name, parseTime)
+-- );
+
+
+delete from `Leaderboard_Player`;
+delete from `Leaderboard_Guild`;
+delete from `Leaderboard_Hell`;
+delete from `underdog`;
+delete from `sayonara`;
+delete from `redbridge`;
+delete from `paragonia`;
+delete from `droplet`;
+delete from `777`;
+delete from `skeleton_skl`;
+delete from `shalom`;
+
+
+
+show tables;
