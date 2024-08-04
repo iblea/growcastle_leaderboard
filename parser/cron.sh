20611af3038e79775a0e90245d66c53f2451637a
iblea <iblea@iasdf.com>
2024-08-04 18:51:38 +0900

    chore: cron script 추가

---
+++ parser/cron.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+curpath=$(dirname "$(realpath $0)")
+cd "$curpath"
+
+JAR_DIR="build/libs/"
+JAR_NAME="parser.jar"
+
+if [ ! -f "${JAR_DIR}/${JAR_NAME}" ]; then
+    echo "${JAR_DIR}/${JAR_NAME} is not exist"
+fi
+
+proc=$(ps -aef | grep "java -jar.*${JAR_NAME}" | grep -v "grep")
+if [ -z "$proc" ]; then
+    cd "$curpath"
+    echo -n "" > output.log
+    nohup ./start.sh >> output.log 2>&1 &
+    echo "start growacastle api parser program"
+else
+    echo "already running"
+fi
