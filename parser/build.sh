6d012c617a05a26166ee43af2b2d629825c344c4
iblea <iblea@iasdf.com>
2024-08-11 00:18:07 +0900

    chore: log4j 파일명 변경, 빌드 스크립트 설정 관련 내용 추가

--- parser/build.sh
+++ parser/build.sh
@@ -14,6 +14,19 @@ JPA_SETTINGS_FILE="src/main/resources/META-INF/persistence.xml"
 if [ ! -f "${JPA_SETTINGS_FILE}" ]; then
     echo "${JPA_SETTINGS_FILE} is not exist"
     echo "Please check the file"
+    echo "========================================="
+    echo "cd src/main/resources/META-INF"
+    echo "cp -r persistence.sample.xml persistence.xml"
+    exit 1
+fi
+
+LOG4J_SETTINGS_FILE="src/main/resources/log4j2.properties"
+if [ ! -f "${LOG4J_SETTINGS_FILE}" ]; then
+    echo "${LOG4J_SETTINGS_FILE} is not exist"
+    echo "Please check the file"
+    echo "========================================="
+    echo "cd src/main/resources"
+    echo "cp -r log4j2.properties.sample log4j2.properties"
     exit 1
 fi
 
