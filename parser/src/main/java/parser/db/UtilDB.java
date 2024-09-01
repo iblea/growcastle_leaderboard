c09c6728a9d9c082657c4ad4928c2940985248af
iblea <iblea@iasdf.com>
2024-09-01 22:47:01 +0900

    feat: DB 관련 유틸 메서드 통합

---
+++ parser/src/main/java/parser/db/UtilDB.java
@@ -0,0 +1,68 @@
+package parser.db;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class UtilDB {
+
+    private static Logger logger = LogManager.getLogger(UtilDB.class);
+
+    public static EntityManager setEntityManager(Database db) {
+        try {
+            EntityManagerFactory emf = db.getEntityManagerFactory();
+            return emf.createEntityManager();
+        } catch (Exception e) {
+            logger.error("setEntityManager error");
+            logger.error(e.getMessage());
+        }
+        return null;
+    }
+
+    public static EntityManager checkEntityManager(Database db, EntityManager em) {
+        if (em == null) {
+            logger.error("EntityManager is null");
+            return setEntityManager(db);
+        }
+        if (!em.isOpen()) {
+            logger.error("EntityManager is closed");
+            return setEntityManager(db);
+        }
+        return em;
+    }
+
+    public static boolean closeEntityManager(EntityManager em) {
+        if (em == null) {
+            logger.error("EntityManager is null");
+            return false;
+        }
+        try {
+            if (em.isOpen()) {
+                em.close();
+            }
+        } catch (Exception e) {
+            logger.error("closeEntityManager error");
+            logger.error(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean transactionRollback(EntityTransaction transaction) {
+        boolean stat = true;
+        try {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+        } catch (Exception e) {
+            logger.error("transactionRollback error");
+            logger.error(e.getMessage());
+            stat = false;
+        }
+        return stat;
+    }
+
+}
