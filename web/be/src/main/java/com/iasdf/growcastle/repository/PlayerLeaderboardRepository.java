68d40d8930c52a62bc550aa4292d59939172a995
iblea <iblea@iasdf.com>
2024-10-04 05:11:29 +0900

    feat: JPA 사용자 정의 인터페이스 구현

--- web/be/src/main/java/com/iasdf/growcastle/repository/PlayerLeaderboardRepository.java
+++ web/be/src/main/java/com/iasdf/growcastle/repository/PlayerLeaderboardRepository.java
@@ -7,12 +7,41 @@ import org.springframework.stereotype.Repository;
 
 import com.iasdf.growcastle.domain.LeaderboardPlayer;
 import com.iasdf.growcastle.domain.MemberPK;
+import com.iasdf.growcastle.domain.QLeaderboardPlayer;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.NonNull;
 
 
 @Repository
-public interface PlayerLeaderboardRepository extends JpaRepository<LeaderboardPlayer, MemberPK>
+public interface PlayerLeaderboardRepository extends JpaRepository<LeaderboardPlayer, MemberPK>, PlayerLeaderboardRepositoryCustom
 {
     @NonNull List<LeaderboardPlayer> findAll();
+}
+
+interface PlayerLeaderboardRepositoryCustom {
+
+    List<LeaderboardPlayer> findsOffset(int limit, int offset);
+}
+
+// @RequiredArgsConstructor
+class PlayerLeaderboardRepositoryCustomImpl implements PlayerLeaderboardRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+    private QLeaderboardPlayer qLeaderboardPlayer = new QLeaderboardPlayer("leaderboard_player");
+
+    public PlayerLeaderboardRepositoryCustomImpl(final JPAQueryFactory queryFactory) {
+        this.queryFactory = queryFactory;
+    }
+
+    @Override
+    public List<LeaderboardPlayer> findsOffset(int limit, int offset)
+    {
+        // List<LeaderboardPlayer> result = null;
+        return queryFactory.select(qLeaderboardPlayer)
+            .from(qLeaderboardPlayer)
+            .offset(offset)
+            .limit(limit)
+            .fetch();
+    }
 }
