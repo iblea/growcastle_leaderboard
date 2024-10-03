34aeec5d62cd86a8ec2716621dc8c5c63a29883c
iblea <iblea@iasdf.com>
2024-10-03 20:51:06 +0900

    feat: SpringConfig 및 PlayerService 매핑

--- web/be/src/main/java/com/iasdf/growcastle/SpringConfig.java
+++ web/be/src/main/java/com/iasdf/growcastle/SpringConfig.java
@@ -1,10 +1,23 @@
 package com.iasdf.growcastle;
 
+import org.springframework.context.annotation.Bean;
 // import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.iasdf.growcastle.repository.PlayerRepository;
+import com.iasdf.growcastle.service.PlayerService;
+
 @Configuration
 public class SpringConfig {
+
+    @Bean
+    public PlayerService playerService() {
+        return new PlayerService(playerRepository());
+    }
+
+    public PlayerRepository playerRepository() {
+        return new PlayerRepository();
+    }
 
 }
