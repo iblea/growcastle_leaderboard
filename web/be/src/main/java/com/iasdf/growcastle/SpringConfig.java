7f0ae44267c6f21975d932eb9b9fbcd4fd31f37f
iblea <iblea@iasdf.com>
2024-10-04 05:13:37 +0900

    feat: JPA 인터페이스 Bean 등록

--- web/be/src/main/java/com/iasdf/growcastle/SpringConfig.java
+++ web/be/src/main/java/com/iasdf/growcastle/SpringConfig.java
@@ -1,5 +1,6 @@
 package com.iasdf.growcastle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 // import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.context.annotation.Bean;
@@ -11,13 +12,19 @@ import com.iasdf.growcastle.service.PlayerLeaderboardService;
 @Configuration
 public class SpringConfig {
 
+    private final PlayerLeaderboardRepository playerLeaderboardRepository;
+
+    @Autowired
+    public SpringConfig(PlayerLeaderboardRepository playerLeaderboardRepository) {
+        this.playerLeaderboardRepository = playerLeaderboardRepository;
+    }
+
     @Bean
     public PlayerLeaderboardService playerLeaderboardService() {
-        return new PlayerLeaderboardService(playerLeaderboardRepository());
+        return new PlayerLeaderboardService(playerLeaderboardRepository);
+        // return new PlayerLeaderboardService(playerLeaderboardRepository());
     }
-
-    public PlayerLeaderboardRepository playerLeaderboardRepository() {
-        return new PlayerLeaderboardRepository();
-    }
-
+    // public PlayerLeaderboardRepository playerLeaderboardRepository() {
+    //     return new PlayerLeaderboardRepositoryTmp();
+    // }
 }
