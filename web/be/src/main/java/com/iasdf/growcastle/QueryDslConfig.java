b085e0001a64a6c7c659dbf67b8c214d1679d64a
iblea <iblea@iasdf.com>
2024-10-04 04:56:24 +0900

    chore: querydsl 추가

---
+++ web/be/src/main/java/com/iasdf/growcastle/QueryDslConfig.java
@@ -0,0 +1,23 @@
+package com.iasdf.growcastle;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+
+@Configuration
+public class QueryDslConfig {
+
+	private final EntityManager em;
+
+	public QueryDslConfig(final EntityManager em) {
+		this.em = em;
+	}
+
+	@Bean
+	public JPAQueryFactory jpaQueryFactory() {
+		return new JPAQueryFactory(em);
+	}
+}
