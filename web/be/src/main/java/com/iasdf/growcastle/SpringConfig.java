ca4e7eb5296dabf012151c75f0394b606d8afa9d
iblea <iblea@iasdf.com>
2024-10-05 22:10:35 +0900

    fix: spring config 불필요한 내용 제거

--- web/be/src/main/java/com/iasdf/growcastle/SpringConfig.java
+++ web/be/src/main/java/com/iasdf/growcastle/SpringConfig.java
@@ -6,25 +6,13 @@ import org.springframework.context.annotation.Bean;
 // import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.iasdf.growcastle.repository.PlayerHistoryRepository;
 import com.iasdf.growcastle.repository.PlayerLeaderboardRepository;
+import com.iasdf.growcastle.service.PlayerHistoryService;
 import com.iasdf.growcastle.service.PlayerLeaderboardService;
 
 @Configuration
-public class SpringConfig {
+public class SpringConfig
+{
 
-    private final PlayerLeaderboardRepository playerLeaderboardRepository;
-
-    @Autowired
-    public SpringConfig(PlayerLeaderboardRepository playerLeaderboardRepository) {
-        this.playerLeaderboardRepository = playerLeaderboardRepository;
-    }
-
-    @Bean
-    public PlayerLeaderboardService playerLeaderboardService() {
-        return new PlayerLeaderboardService(playerLeaderboardRepository);
-        // return new PlayerLeaderboardService(playerLeaderboardRepository());
-    }
-    // public PlayerLeaderboardRepository playerLeaderboardRepository() {
-    //     return new PlayerLeaderboardRepositoryTmp();
-    // }
 }
